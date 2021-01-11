f5dfcff20e49775a9bedf5dd27f645de080cf74a
ivanhular <ivanhular.dev@gmail.com>
2021-01-12 00:14:12 +0800

    1/12/2021

---
+++ build/precache-manifest.07db50c87b9082c09c38f2816ad0b067.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d72d0952d01ab58ec5ceb547211058",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45dc36e325862ff88baa",
+    "url": "/static/css/2.1f6a5dad.chunk.css"
+  },
+  {
+    "revision": "808d2c0f4b6e9b4d0c29",
+    "url": "/static/css/main.47acbac4.chunk.css"
+  },
+  {
+    "revision": "45dc36e325862ff88baa",
+    "url": "/static/js/2.197952bb.chunk.js"
+  },
+  {
+    "revision": "4258a4c46b7ec853e9d53822f8c8f49e",
+    "url": "/static/js/2.197952bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808d2c0f4b6e9b4d0c29",
+    "url": "/static/js/main.5148ae62.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "06bdaff1e11a353be05364b7199a3f16",
+    "url": "/static/media/COD.06bdaff1.png"
+  },
+  {
+    "revision": "45ab99acdf8230130e97e25030e73b12",
+    "url": "/static/media/jnt.45ab99ac.png"
+  },
+  {
+    "revision": "43f57a8873249d77016722b68b560ad9",
+    "url": "/static/media/logo.43f57a88.png"
+  },
+  {
+    "revision": "dd0370ca1ca790809517e5a753fb1856",
+    "url": "/static/media/mobile-logo.dd0370ca.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7ffbb6a65d542dc91897c2683dd8863e",
+    "url": "/static/media/step_3_delete.7ffbb6a6.jpg"
+  },
+  {
+    "revision": "a96b142c27b80040f87a121d929c82bb",
+    "url": "/static/media/visa-master.a96b142c.png"
+  }
+]);
